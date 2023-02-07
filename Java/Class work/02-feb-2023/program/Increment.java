2281895e95d5b520d9705a73a5655f6b68e33145
Malinibalasubramaniyan <93629145+Malinibalasubramaniyan@users.noreply.github.com>
2023-02-07 20:10:42 +0530

    Add files via upload

---
+++ Java/Class work/02-feb-2023/program/Increment.java
@@ -0,0 +1,26 @@
+class Increment
+{
+    public static void main(String[]args)
+    {
+        int i=0;
+        System.out.println("Increment And Decrement Operators");
+        System.out.println(i++);//0 Post
+      System.out.println(++i);//2 Pre
+         System.out.println(i--);//2 POst
+          System.out.println(--i);//0 Pre
+
+   System.out.println("compator operators");
+    
+    int num1=20;
+    int num2=30;
+     int num3=40;
+      int num4=50;
+       int num5=20;
+       System.out.println(num1==num2);
+       System.out.println(num1>=num2);
+       System.out.println(num1<=num2);
+       System.out.println(num1!=num2);
+        System.out.println(num1==num5);
+      
+    }
+}
